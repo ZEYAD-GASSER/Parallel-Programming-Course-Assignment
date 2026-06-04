df6c8babb2bd1860b76cd93f72d2801345588663
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 01:25:57 +0300

    Update Atomic_Operation.cpp

--- Source_Code/Atomic_Operation.cpp
+++ Source_Code/Atomic_Operation.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <omp.h>
+using namespace std ;
+// Shared global accumulator variable
+double X = 0.0;
 
+double DOIT() {
+    return 1.5;
+}
+
+double big_ugly(double B) {
+    return B * 2.5;
+}
+
+int main() {
+    omp_set_num_threads(4);
+
+    #pragma omp parallel
+    {
+        // Local private variables for each thread
+        double tmp, B;
+
+        B = DOIT();
+        tmp = big_ugly(B);
+
+        // Atomic synchronization: Safely update shared variable X
+        #pragma omp atomic
+        X += tmp;
+    }
+
+    cout << "Result (X): " << X << "\n";
+    return 0;
+}
