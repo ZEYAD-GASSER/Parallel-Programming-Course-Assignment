31940fbf036ec761ee7dd64d0f9fdb2baf94dfa9
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 01:15:11 +0300

    Update Removing_Loop_Dependencies.cpp

--- Source_Code/Removing_Loop_Dependencies.cpp
+++ Source_Code/Removing_Loop_Dependencies.cpp
@@ -1,1 +1,25 @@
+#include <omp.h>
+#include <iostream>
 
+using namespace std ;
+const int MAX = 10;
+
+int big(int j) {
+    return j * 2;
+}
+
+int main() {
+    int A[MAX];
+
+    #pragma omp parallel for
+    for (int i = 0; i < MAX; i++) {
+        int j = 5 + 2 * (i + 1);
+        A[i] = big(j);
+    }
+
+    for (int i = 0; i < MAX; i++) {
+        cout << "A[" << i << "] = " << A[i] << "\n";
+    }
+
+    return 0;
+}
