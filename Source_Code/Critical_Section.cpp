f90aaa2dcac5e016d10b672dab6469468861d170
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 00:42:10 +0300

    Update Critical_Section.cpp

--- Source_Code/Critical_Section.cpp
+++ Source_Code/Critical_Section.cpp
@@ -1,1 +1,28 @@
+#include <omp.h>
+#include <iostream>
+using namespace std ;
 
+const int niters = 100 ;
+float big_job(int i) {
+    return (float)(i)* 2.0f;
+}
+float consume(float B) {
+    return B + 1.0f;
+}
+int main ()
+{
+        float res;
+        #pragma omp parallel
+        {
+                float B;  int i, id, nthrds;
+                id = omp_get_thread_num();
+                nthrds = omp_get_num_threads();
+                for(i=id;i<niters;i+=nthrds){
+                        B = big_job(i);
+                #pragma omp critical
+                      res += consume (B);
+                }
+        }
+        cout<< " Result : " << res <<endl ;
+        return 0 ;
+}
