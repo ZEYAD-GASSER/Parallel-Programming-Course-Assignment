3bdfe95a7b216729fc5e7b968a48c0fdf45173ad
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 01:39:09 +0300

    Update Communication_Bandwidth.cpp

--- Source_Code/Communication_Bandwidth.cpp
+++ Source_Code/Communication_Bandwidth.cpp
@@ -1,1 +1,86 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define NUMBER_OF_TESTS 10    // For more reliable average results
+
+int main(int argc, char* argv[])
+{
+    double *buf;
+    int rank, numprocs;
+    int n;
+    double t1, t2;
+    int j, k, nloop;
+    MPI_Status status;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+    if (numprocs != 2)
+    {
+        printf("The number of processes must be two!!\n");
+        MPI_Finalize();
+        return 0;
+    }
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0)
+    {
+        printf("\tn\ttime [sec]\tRate [Mb/sec]\n");
+    }
+
+    for (n = 1; n < 100000000; n *= 2)
+    {
+        nloop = 1000000 / n;
+
+        if (nloop < 1)
+            nloop = 1;
+
+        buf = (double *)malloc(n * sizeof(double));
+
+        if (!buf)
+        {
+            printf("Could not allocate message buffer of size %d\n", n);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        for (k = 0; k < NUMBER_OF_TESTS; k++)
+        {
+            if (rank == 0)
+            {
+                t1 = MPI_Wtime();
+
+                for (j = 0; j < nloop; j++)
+                {
+                    MPI_Send(buf, n, MPI_DOUBLE, 1, k, MPI_COMM_WORLD);
+                }
+
+                t2 = (MPI_Wtime() - t1) / nloop;
+            }
+            else if (rank == 1)
+            {
+                for (j = 0; j < nloop; j++)
+                {
+                    MPI_Recv(buf, n, MPI_DOUBLE, 0, k,
+                             MPI_COMM_WORLD, &status);
+                }
+            }
+        }
+
+        if (rank == 0)
+        {
+            double bandwidth;
+            bandwidth = n * sizeof(double) * 1.0e-6 * 8 / t2;
+
+            printf("\t%10d\t%10.8f\t%8.2f\n",
+                   n, t2, bandwidth);
+        }
+
+        free(buf);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
