bbc8e24e6adb62f00baf1e783c0324115265e3e8
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 01:19:20 +0300

    Update Reduction_Clause.cpp

--- Source_Code/Reduction_Clause.cpp
+++ Source_Code/Reduction_Clause.cpp
@@ -1,1 +1,28 @@
 
+#include <omp.h>
+#include <iostream>
+
+const int MAX = 100;
+using namespace std ;
+int main() {
+    double ave = 0.0;
+    double A[MAX];
+
+    // initialize the array elements with dummy data
+    for (int i = 0; i < MAX; i++) {
+        A[i] = static_cast<double>(i + 1);
+    }
+
+    // parallel loop with an addition reduction on the 'ave' variable
+    #pragma omp parallel for reduction(+:ave)
+    for (int i = 0; i < MAX; i++) {
+        ave += A[i];
+    }
+
+    // calculate the final average value
+    ave = ave / MAX;
+
+    cout << "Calculated Average (ave): " << ave << "\n";
+
+    return 0;
+}
