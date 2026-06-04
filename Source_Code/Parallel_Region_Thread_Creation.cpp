1e9f98d4154c46d76bfb071fac0e00ee239e0a27
Zeyad Gasser <141816908+ZEYAD-GASSER@users.noreply.github.com>
2026-06-05 00:22:27 +0300

    Update Parallel_Region_Thread_Creation.cpp

--- Source_Code/Parallel_Region_Thread_Creation.cpp
+++ Source_Code/Parallel_Region_Thread_Creation.cpp
@@ -1,1 +1,18 @@
-
+#include "omp.h"
+#include <stdio.h>
+void pooh(int thread_id, double *array) {
+    // A sample implementation of the lecture's dummy function
+    printf("Thread %d processing array A at address: %p\n", thread_id, (void*)array);
+}
+int main()
+{
+        // here I set the max number of threads to 4
+        double A[1000] ;
+        omp_set_num_threads(4);
+        #pragma omp parallel
+        {
+                int ID = omp_get_thread_num();
+                pooh(ID, A);
+        }
+        return 0 ;
+}
